9d9a6794e258cddb9ac9851a77609a860a0c7d60
Kazan Glenn <kazan@kazan.uk>
2020-02-06 15:25:26 +0000

    cleanup

--- src/world/World.jsx
+++ src/world/World.jsx
@@ -260,7 +260,7 @@ const Batch = withPixiApp(class extends React.PureComponent {
 
   // TODO - deconstruct into smaller functions
   logic = (items) => {
-
+    // build arrays once
     var all = this.deaths(items); // check once over all items
     var algae = all.filter(item => item.type === "algae");
     var bugs = all.filter(item => item.type === "bug");
@@ -291,14 +291,9 @@ const Batch = withPixiApp(class extends React.PureComponent {
         }
 
         // check overlap - if overlap, do not breed (do no add offpsring) until space
-        // var isContact = items.filter(inner => inner.type === "algae").some((algae) => {
-        //   contact(offspring, algae);
-        // })
         let isContact = false;
-        // let algae = items.filter(inner => inner.type === "algae");
         for(i = 0; i < algae.length; i++) { // don't use foreach so can break
           if(contact(offspring, algae[i])) {
-            // console.log("contact", i, algae.length, offspring, algae[i])
             isContact = true;
             break;
           }
@@ -362,148 +357,6 @@ const Batch = withPixiApp(class extends React.PureComponent {
       item.energy = Math.max(0, item.energy - (2 + Math.ceil(item.speed))); // slow energy loss with movement, will need to eat
     })
 
-    // TODO - reseed algae
-    // items.forEach((item, i) => {
-
-    //   // check if still has energy
-    //   if(item.energy === 0) {
-    //     items.splice(i, 1); // other is at position i
-    //     // console.log(item.type,"death");
-    //   }
-
-    //   else {
-
-    //     // check if enough energy for bug to breed, artifical break on population
-    //     if(item.type === "bug" && item.energy >= item.breedThreshold && items.filter(item => item.type === "bug").length <= 200) {
-    //       tracker.totalBugs++;
-    //       // this.setState(tracker.totalBugs++);
-    //       item.energy = Math.round(item.energy/2) - 100; // half, plus breeding cost
-    //       let offspring = Object.assign({}, item); // empty object to receive contents of item
-    //       // offspring.speed = (2 + Math.random() * 6) * 0.5; // keep same speed unless mutation
-    //       offspring.direction = Math.random() * Math.PI * 2; // new heading
-    //       // 1 in n chance of a mutuation
-    //       if(Math.floor(Math.random() * 10) === 0) {
-    //         tracker.totalSpecies++;
-    //         // this.setState(tracker.totalSpecies++);
-    //         offspring.turningSpeed = item.turningSpeed + (Math.random() * 0.2 - 0.1);
-    //         offspring.speed = item.speed + Math.floor(Math.random() * 3); // (1 + Math.random() * 10) * 0.5; // new speed
-    //         offspring.tint = Math.random() * 0xFFFFFF; // new colour to indicate change
-    //         offspring.breedThreshold += item.breedThreshold + Math.floor(Math.random() * 50) - 25; // variable breed speed, up or down
-    //       }
-    //       items.push(offspring); // add adjusted copy
-    //       // console.log(item.type,"birth");
-    //     }
-
-    //     // algae breed - if energy doubled, split
-    //     // TODO - make constants for limits
-    //     if(item.type === "algae" && item.energy >= 100 && items.filter(item => item.type === "algae").length <= 800) {
-    //       let offspring = Object.assign({}, item); // empty object to receive contents of item
-
-    //       let index = Math.floor(Math.random() * positions.length);
-    //       // randomness creates drift from pure grid, lines reminisent of algae
-    //       offspring.x += positions[index][0] + Math.floor(Math.random() * 8) - 4;
-    //       offspring.y += positions[index][1] + Math.floor(Math.random() * 8) - 4;
-
-    //       // can push this into a function - used for bugs as well
-    //       if (offspring.x < this.bounds.x) {
-    //         offspring.x += this.bounds.width;
-    //       }
-    //       else if (offspring.x > this.bounds.x + this.bounds.width) {
-    //         offspring.x -= this.bounds.width;
-    //       }
-    //       if (offspring.y < this.bounds.y) {
-    //         offspring.y += this.bounds.height;
-    //       }
-    //       else if (offspring.y > this.bounds.y + this.bounds.height) {
-    //         offspring.y -= this.bounds.height;
-    //       }
-
-    //       // check overlap - if overlap, do not breed (do no add offpsring) until space
-    //       // var isContact = items.filter(inner => inner.type === "algae").some((algae) => {
-    //       //   contact(offspring, algae);
-    //       // })
-    //       let isContact = false;
-    //       let algae = items.filter(inner => inner.type === "algae");
-    //       for(i = 0; i < algae.length; i++) { // don't use foreach so can break
-    //         if(contact(offspring, algae[i])) {
-    //           // console.log("contact", i, algae.length, offspring, algae[i])
-    //           isContact = true;
-    //           break;
-    //         }
-    //       }
-          
-    //       if(!isContact) {
-    //         // only take energy away when space to breed
-    //         item.energy = Math.round(item.energy/2); // half
-    //         offspring.energy = Math.round(item.energy/2); // half
-    //         items.push(offspring); // add adjusted copy
-    //       }
-    //     }
-
-    //     // check if bug has contact with others, when can take energy from them
-    //     // could at least limit test by reasonable proximity
-    //     if(item.type === "bug"){
-    //       items.forEach((other, j) => {
-    //         // don't test against self, based on position of old value
-    //         if(item.tint !== other.tint && item.x !== other.x && item.y !== other.y) {
-    //           if(contact(item, other)) {
-    //             // TODO - switch to bump library, more sophisticated contact detection
-    //             // TODO - avoid overlap
-    //             item.direction += (Math.random() - 0.5) * 0.5; // avoidance, irrelevant for algae
-    //             // consume the contacted bug or algae
-    //             if(other.energy > 0) {
-    //               item.energy += Math.min(20, other.energy); // take what there is, or 20
-    //               other.energy = Math.max(0, other.energy - 20); // reduce to 0, or by 20
-    //             }
-    //           }
-    //         }
-    //       })  
-    //     }
-
-    //     // simulate photosynthesis; algae gather energy slowly from environment
-    //     if(item.type === "algae") {
-    //       // item.energy += 1; 
-    //       item.energy = Math.min(100, item.energy + 1); // put a cap on energy stored
-    //       // TO SLOW - but found energy bug ...
-    //       // if(item.energy <= 75) {
-    //       //   item.tint = 0x9ACD32;
-    //       // }
-    //       // else {
-    //       //   item.tint = 0x2E8B57;
-    //       // }
-    //     }
-
-    //     // TODO - work out why coord changes here work but not in tick
-    //     if(item.type === "bug") {
-    //       // move the item; this is where ML/AI logic can go - just random adjustments for now
-    //       item.x = item.x + Math.sin(item.direction) * (item.speed * item._s);
-    //       item.y = item.y + Math.cos(item.direction) * (item.speed * item._s);
-    //       item.rotation = -item.direction + Math.PI;
-    //       item.direction = item.direction + item.turningSpeed * (Math.random() - 0.5) * 0.5;
-    //         // speed impacts energy loss rate, slower = lower
-    //       item.energy = Math.max(0, item.energy - (2 + Math.ceil(item.speed))); // slow energy loss with movement, will need to eat
-    //     }
-
-    //     // // check bounds of environment not exceeded, wrap around if so
-    //     // if (item.x < this.bounds.x) {
-    //     //   item.x += this.bounds.width
-    //     // }
-    //     // else if (item.x > this.bounds.x + this.bounds.width) {
-    //     //   item.x -= this.bounds.width
-    //     // }
-    //     // if (item.y < this.bounds.y) {
-    //     //   item.y += this.bounds.height
-    //     // }
-    //     // else if (item.y > this.bounds.y + this.bounds.height) {
-    //     //   item.y -= this.bounds.height
-    //     // }
-        
-    //   }
-
-      
-    // });
-
-    // return items;
     return bugs.concat(algae);
   }
 
