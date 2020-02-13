591dde9df60f2f2ff43373a196168fb9e80ee055
Kazan Glenn <kazan@kazan.uk>
2020-02-13 10:39:48 +0000

    Updates

---
+++ precache-manifest.b099254b2846fdc1ee0b8e107c894bf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e05e01f9463b561c541ba950f6c6f3",
+    "url": "/bugs2020/index.html"
+  },
+  {
+    "revision": "f9bc575cda752e10c40e",
+    "url": "/bugs2020/static/css/main.ec2ae3ec.chunk.css"
+  },
+  {
+    "revision": "a36121c76a924aaaaeda",
+    "url": "/bugs2020/static/js/2.749f7278.chunk.js"
+  },
+  {
+    "revision": "b2cc28a13881afae4bc7319c9e9134ea",
+    "url": "/bugs2020/static/js/2.749f7278.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9bc575cda752e10c40e",
+    "url": "/bugs2020/static/js/main.ee3bb103.chunk.js"
+  },
+  {
+    "revision": "9dc2421d13709e462e83",
+    "url": "/bugs2020/static/js/runtime-main.356ecdd1.js"
+  },
+  {
+    "revision": "bfb2339da053eb622aed13a65acedc4e",
+    "url": "/bugs2020/static/media/bugs2020.bfb2339d.png"
+  }
+]);
